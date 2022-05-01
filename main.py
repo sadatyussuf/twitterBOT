50fe516c1defc57014f1eafbcf6359696304e126
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-05-01 10:57:01 +0000

    refactoring prev code and added logging msg

--- main.py
+++ main.py
@@ -1,46 +1,65 @@
 import tweepy
 from dotenv import load_dotenv
+import requests
 
 import os
+import logging
+import time
 
 load_dotenv()
+# logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG,filename="msg.log",filemode='w',
+format='%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s')
 
 CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
 
-# Authenticate to Twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN,ACCESS_SECRET)
 
 
-#? Create API object
+#* Create API object
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-# print(api.me())
-
-#? Verify Credentials
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
-
-#? Fetch first 20 tweets from Timeline
-# timeline = api.home_timeline(count=2)
-# for tweet in timeline:
-#     print(f'{tweet.user.name} said {tweet.text}')
-
-
-# Fetch Particular User
-user = api.get_user(screen_name="yussufSadat")
-print(user.screen_name)
-print(user.description)
-print(user.location)
-print(user.followers_count)
+#* Authenticate to Twitter
+def authenticate():
+    # try:
+    #     api.verify_credentials()
+    #     logging.info("Authentication Success!")
+    #     return True
+    # except:
+    #     logging.exception("Failed Authentication")
+    #     return False
+    if not api.verify_credentials():
+        logging.error("Authentication Failed",exc_info=True)
+        return False
+    else:
+        logging.info("Authentication Success!")
+        return True
 
 
-# Search a keyword for most recent public tweets  in english
-for tweet in api.search_tweets(q='knust',lang="en",count=5):
-    print(f"{tweet.user.name} : {tweet.text}")
+def main():
+    if authenticate():
+        print("do stuff")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+# url = "https://ghana-shs-api.herokuapp.com/api/shs"
+# resp = requests.get(url)
+
+# # print(r.content)
+
+# api_resp = resp.json()
+
+# for i in range(0,5):
+#     print(api_resp[i])
+#     time.sleep(10)
